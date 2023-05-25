624e3bd588827851eeab136db9b757634b34e79a
dFxPhoeniX <waltercurelea@yahoo.com>
2023-05-25 03:15:30 +0300

    Upgraded all code to TS

--- dist/mojang/rest/MojangRestAPI.js
+++ dist/mojang/rest/MojangRestAPI.js
@@ -1,7 +1,11 @@
 "use strict";
 var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
 }) : (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
     o[k2] = m[k];
@@ -286,7 +290,7 @@ exports.MojangRestAPI = MojangRestAPI;
 MojangRestAPI.logger = LoggerUtil_1.LoggerUtil.getLogger('Mojang');
 MojangRestAPI.TIMEOUT = 2500;
 MojangRestAPI.AUTH_ENDPOINT = 'https://authserver.mojang.com';
-MojangRestAPI.STATUS_ENDPOINT = 'https://mojang-status.minecraft.evopixel.ro';
+MojangRestAPI.STATUS_ENDPOINT = 'https://minecraft.evopixel.ro/launcher/mojang-status';
 MojangRestAPI.authClient = got_1.default.extend({
     prefixUrl: MojangRestAPI.AUTH_ENDPOINT,
     responseType: 'json',
